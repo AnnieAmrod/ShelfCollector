1ab94ce36a0542af498f6c91b8fca6f10d9a127c
Miriam Durán García <93394599+AnnieAmrod@users.noreply.github.com>
2024-07-28 12:01:06 +0200

    Update settings.py

--- shelfCollector/shelfCollector/settings.py
+++ shelfCollector/shelfCollector/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure--eduf*^r)wk4!b@ogxn2_woj84dn*n%0xz#tylek6p81bgsm-7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
